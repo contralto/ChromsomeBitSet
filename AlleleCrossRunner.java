ce1ce3d3e9c3cd9f68ef8b995c2be1b416d8ab30
Claudia <claudia.gonciulea@gmail.com>
2018-09-23 17:54:37 -0400

    > Updated project to consider multiple genes

--- AlleleCrossRunner.java
+++ AlleleCrossRunner.java
@@ -4,9 +4,17 @@ public class AlleleCrossRunner {
         Chromosome parent1 = Chromosome.generate();
         Chromosome parent2 = Chromosome.generate();
 
-        Chromosome child = Chromosome.cross(parent1, parent2);
+        Chromosome child1 = Chromosome.cross(parent1, parent2);
 
-        System.out.println(parent1.toString() + " " + parent2.toString());
-        System.out.println(child.toString());
+        Chromosome parent3 = new Chromosome(2, new boolean[]{false, false, true, false});
+        Chromosome parent4 = new Chromosome(2, new boolean[]{true, false, true, true});
+
+        Chromosome child2 = Chromosome.cross(parent3, parent4);
+
+        System.out.println("p1: " + parent1.toString() + ", p2: " + parent2.toString());
+        System.out.println("c1: " + child1.toString());
+
+        System.out.println("p3: " + parent3.toString() + ", p4: " + parent4.toString());
+        System.out.println("c2: " + child2.toString());
     }
 }
--- Chromosome.java
+++ Chromosome.java
@@ -4,21 +4,39 @@ import java.lang.Math;
 
 public class Chromosome {
     private BitSet chromo;
+    private int genecount;
+    private static int gc;
     public Chromosome() {
         chromo = new BitSet(2);
+        genecount = 1;
+        gc = 1;
+    }
+
+    public Chromosome(int numgenes) {
+        chromo = new BitSet(numgenes * 2);
+        genecount = numgenes;
+        gc = numgenes;
     }
 
     public Chromosome(boolean isDominant1, boolean isDominant2) {
         chromo = new BitSet(2);
         chromo.set(0, isDominant1);
         chromo.set(1, isDominant2);
+        genecount = 1;
+        gc = 1;
     }
 
-    public Chromosome(int numgenes) {
+    public Chromosome(int numgenes, boolean[] ray) {
         chromo = new BitSet(numgenes * 2);
+        genecount = numgenes;
+        gc = numgenes;
+        for (int i = 0; i < numgenes; i += 2) {
+            chromo.set(i, ray[i]);          //represents allele 1 of a certain gene
+            chromo.set(i + 1, ray[i + 1]);  //represents allele 2 of a certain gene
+        }
     }
 
-    public static boolean randomBoolean() {
+    private static boolean randomBoolean() {
         return Math.random() < 0.5;
     }
 
@@ -27,20 +45,20 @@ public class Chromosome {
     }
 
     public static Chromosome cross(Chromosome parent1, Chromosome parent2) {
-        Chromosome child = new Chromosome();
-        boolean chooseA1 = Chromosome.randomBoolean();
-        boolean chooseA2 = Chromosome.randomBoolean();
-        if (chooseA1) {
-            child.set(0, parent1.get(0));
-        }
-        else {
-            child.set(0, parent1.get(1));
-        }
-        if (chooseA2) {
-            child.set(1, parent2.get(0));
-        }
-        else {
-            child.set(1, parent2.get(1));
+        Chromosome child = new Chromosome(gc);
+        for (int i = 0; i < parent1.getGeneCount(); i += 2) {
+            boolean chooseA1 = Chromosome.randomBoolean();
+            boolean chooseA2 = Chromosome.randomBoolean();
+            if (chooseA1) {
+                child.set(i, parent1.getAllele(i));
+            } else {
+                child.set(i, parent1.getAllele(i + 1));
+            }
+            if (chooseA2) {
+                child.set(i + 1, parent2.getAllele(i));
+            } else {
+                child.set(i + 1, parent2.getAllele(i + 1));
+            }
         }
         return child;
     }
@@ -49,11 +67,19 @@ public class Chromosome {
         chromo.set(spot, allele);
     }
 
-    private boolean get(int spot) {
+    private boolean getAllele(int spot) {
         return chromo.get(spot);
     }
 
+    private static int getGeneCount() {
+        return gc;
+    }
+
     public String toString() {
-        return "[" + chromo.get(0) + ", " + chromo.get(1) + "]";
+        String printer = "";
+        for (int i = 0; i < genecount * 2; i++) {
+            printer += getAllele(i) + ", ";
+        }
+        return "[" + printer.substring(0, printer.length() - 2) + "]";
     }
 }
